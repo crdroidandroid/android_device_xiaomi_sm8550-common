854bdb409e864fcd6dec3f8389059fb43d20ad90
kenway214 <kenway214@outlook.com>
2025-03-10 16:03:06 +0530

    sm8450-common: parts: Introduce PowerTools v3.0
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -31,6 +31,7 @@ import android.view.Display.HdrCapabilities;
 
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.doze.PocketService;
+import org.lineageos.settings.powertools.PowerProfileTileService;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.thermal.ThermalTileService;
 import org.lineageos.settings.refreshrate.RefreshUtils;
@@ -81,12 +82,17 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         ThermalUtils.getInstance(context).startService();
         context.startServiceAsUser(new Intent(context, ThermalTileService.class), UserHandle.CURRENT);
 
+<<<<<<< HEAD
         // Start Refresh Rate Service
         RefreshUtils.startService(context);
 
         // Start Pocket Mode Service
         PocketService.startService(context);
 
+=======
+        // Start Power Profile Tile Service
+        context.startServiceAsUser(new Intent(context, PowerProfileTileService.class), UserHandle.CURRENT);
+>>>>>>> 83244d3 (sm8450-common: parts: Introduce PowerTools v3.0)
     }
 
     private void overrideHdrTypes(Context context) {
--- parts/src/org/lineageos/settings/TileHandlerActivity.java
+++ parts/src/org/lineageos/settings/TileHandlerActivity.java
@@ -37,6 +37,8 @@ import org.lineageos.settings.refreshrate.RefreshActivity;
 import org.lineageos.settings.refreshrate.RefreshTileService;
 import org.lineageos.settings.gamebar.GameBarSettingsActivity;
 import org.lineageos.settings.gamebar.GameBarTileService;
+import org.lineageos.settings.powertools.PowerProfileTileService;
+import org.lineageos.settings.powertools.PowertoolsActivity;
 
 public final class TileHandlerActivity extends Activity {
     private static final String TAG = "TileHandlerActivity";
@@ -50,6 +52,7 @@ public final class TileHandlerActivity extends Activity {
         TILE_ACTIVITY_MAP.put(ThermalTileService.class.getName(), ThermalSettingsActivity.class);
         TILE_ACTIVITY_MAP.put(RefreshTileService.class.getName(), RefreshActivity.class);
         TILE_ACTIVITY_MAP.put(GameBarTileService.class.getName(), GameBarSettingsActivity.class);
+        TILE_ACTIVITY_MAP.put(PowerProfileTileService.class.getName(), PowertoolsActivity.class);
     }
 
     @Override
