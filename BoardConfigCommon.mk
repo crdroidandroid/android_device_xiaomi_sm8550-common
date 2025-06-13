2ba0fb64e88d55cad1d0e1c2ab6a6ff01079a6c8
xt0032rus <andrey.denikin9@mail.com>
2025-06-13 20:59:28 +0300

    sm8550-common: Panic to recovery, not bootloader

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -76,6 +76,7 @@ BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
 
 BOARD_KERNEL_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000 disable_dma32=on swinfo.fingerprint=$(LINEAGE_VERSION) mtdoops.fingerprint=$(LINEAGE_VERSION)
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 
 BOARD_BOOTCONFIG := androidboot.hardware=qcom androidboot.memcg=1 androidboot.usbcontroller=a600000.dwc3 androidboot.console=ttyMSM0
 
