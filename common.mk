e156a755bc5aa07a7306521eeaaf65c50d68ace8
Leviscape <leviscape.xyz@gmail.com>
2025-10-22 14:48:46 +0530

    sm8550-common: Drop qesdk, qesdhal blobs and ModemManager
    Change-Id: Ic1faf78f442d57f6c5129e4998dc3194cb9a0fab

--- common.mk
+++ common.mk
@@ -242,11 +242,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libcodec2_hidl_shim
 
-# ModemManager
-PRODUCT_PACKAGES += \
-    libqesdk_manager_32bit_stub \
-    libqesdk2_0_32bit_stub
-
 # Network
 PRODUCT_PACKAGES += \
     libnetfilter_conntrack \
