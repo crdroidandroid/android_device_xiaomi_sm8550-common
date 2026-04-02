655515723a099009f0684a232b1e5eebd814101d
WenHao2130 <wenhao2130@outlook.com>
2026-04-02 23:05:15 +0800

    sm8550-common: Add batterysecret to usb group
    * Following the change in d610020984849c0814e44d3c8e6dcf6271b9a3a1
      the group ownership of /sys/class/typec/port0 was set to 'usb'.
    * This mismatch caused batterysecret to trigger a dac_override
      denial, as it lacked the proper group permissions to access
      the USB nodes, subsequently breaking fast charging:
    
      avc: denied { dac_override } for capability=1 scontext=u:r:batterysecret:s0
      tcontext=u:r:batterysecret:s0 tclass=capability permissive=0
    
    * Add batterysecret to the 'usb' group to restore access and fix charging.
    
    Change-Id: I6bd9bbe5d9b73ba4c9ae18ac2606d00fa1950716
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- extract-files.py
+++ extract-files.py
@@ -142,6 +142,8 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/libaudiocloudctrl.so',
     ): blob_fixup()
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
+    'vendor/etc/init/hw/init.batterysecret.rc' : blob_fixup()
+        .regex_replace('group system system wakelock', 'group system system usb wakelock'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
