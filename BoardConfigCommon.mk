89f64bbaee79bc5112da74f4a6d03c82adeb7ce0
Sebastiano Barezzi <seba@sebaubuntu.dev>
2026-04-06 22:46:06 +0800

    sm8550-common: Use hardware/xiaomi FCM
    Change-Id: Ib1dcdfe20ef5ffaf1b3b407b47790ee029a2755f
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -194,10 +194,10 @@ TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
 # VINTF
-DEVICE_MATRIX_FILE := $(COMMON_PATH)/configs/vintf/compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(COMMON_PATH)/configs/vintf/framework_matrix.xml \
-    $(COMMON_PATH)/configs/vintf/product_framework_matrix.xml \
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 
 DEVICE_MANIFEST_FILE := \
     $(COMMON_PATH)/configs/vintf/manifest_kalama.xml \
